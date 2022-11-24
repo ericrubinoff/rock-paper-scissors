ef9a09074a6ed65d57fb7472ebc6f59b7d34e8ec
Eric Rubinoff <ericrubinoff@gmail.com>
2022-11-24 11:29:24 -0500

    Add buttons and events, remove console logs

--- javascript.js
+++ javascript.js
@@ -1,17 +1,4 @@
-/* Randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. */
-
-function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
-  if (randomNumber == 1) {
-    return "rock";
-  } else if (randomNumber == 2) {
-    return "paper";
-  } else {
-    return "scissors";
-  }
-}
-
-/* Strings */
+// Strings to be returned on each play
 
 const rockScissors = "Rock beats Scissors, you win!";
 const rockPaper = "Paper beats Rock, you lose!";
@@ -24,72 +11,116 @@ const playerWins = "You are the winner!";
 const computerWins = "The computer wins, you lose!";
 const keepPlaying = "It's a draw, continue playing!";
 
-// Single Round
+// Generate computer's random choice
+
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  if (randomNumber == 1) {
+    return "rock";
+  } else if (randomNumber == 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+// Initial Score
 
 let playerScore = 0;
 let computerScore = 0;
 
+// Single Round
+
 function playRound(playerSelection, computerSelection) {
   switch (true) {
     case playerSelection == "rock" && computerSelection == "scissors":
-      playerScore++;
-      console.log(rockScissors);
+      playerScore++; // Add score to player
+      document.getElementById("printPlayerSelection").innerHTML =
+        playerSelection; // Print player's selection
+      document.getElementById("printComputerSelection").innerHTML =
+        computerSelection; // Print computer's selection
+      document.getElementById("gameOutcome").innerHTML = rockScissors; // Print game outcome
       break;
     case playerSelection == "rock" && computerSelection == "paper":
       computerScore++;
-      console.log(rockPaper);
+      document.getElementById("printPlayerSelection").innerHTML =
+        playerSelection;
+      document.getElementById("printComputerSelection").innerHTML =
+        computerSelection;
+      document.getElementById("gameOutcome").innerHTML = rockPaper;
       break;
     case playerSelection == "paper" && computerSelection == "rock":
       playerScore++;
-      console.log(paperRock);
+      document.getElementById("printPlayerSelection").innerHTML =
+        playerSelection;
+      document.getElementById("printComputerSelection").innerHTML =
+        computerSelection;
+      document.getElementById("gameOutcome").innerHTML = paperRock;
       break;
     case playerSelection == "paper" && computerSelection == "scissors":
       computerScore++;
-      console.log(paperScissors);
+      document.getElementById("printPlayerSelection").innerHTML =
+        playerSelection;
+      document.getElementById("printComputerSelection").innerHTML =
+        computerSelection;
+      document.getElementById("gameOutcome").innerHTML = paperScissors;
       break;
     case playerSelection == "scissors" && computerSelection == "rock":
       computerScore++;
-      console.log(scissorsRock);
+      document.getElementById("printPlayerSelection").innerHTML =
+        playerSelection;
+      document.getElementById("printComputerSelection").innerHTML =
+        computerSelection;
+      document.getElementById("gameOutcome").innerHTML = scissorsRock;
       break;
     case playerSelection == "scissors" && computerSelection == "paper":
       playerScore++;
-      console.log(scissorsPaper);
+      document.getElementById("printPlayerSelection").innerHTML =
+        playerSelection;
+      document.getElementById("printComputerSelection").innerHTML =
+        computerSelection;
+      document.getElementById("gameOutcome").innerHTML = scissorsPaper;
       break;
     default:
-      console.log(draw);
+      document.getElementById("printPlayerSelection").innerHTML =
+        playerSelection;
+      document.getElementById("printComputerSelection").innerHTML =
+        computerSelection;
+      document.getElementById("gameOutcome").innerHTML = draw;
   }
 }
 
-/* Play 5 Rounds and announce winner */
+// Events
 
-function game() {
-  for (let roundNumber = 0; roundNumber < 5; roundNumber++) {
-    
-    getComputerChoice();
-    const computerSelection = getComputerChoice();
-    
-    
-    const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+// Play single round and print Scores
+function playRoundPrintValue(playerSelection) {
+  playRound(playerSelection, getComputerChoice());
+  document.getElementById("printPlayerScore").innerHTML = playerScore;
+  document.getElementById("printComputerScore").innerHTML = computerScore;
+  announceWinner();
+}
 
-    playRound(playerSelection, computerSelection);
+// Play individual round by pressing button
+let btnRock = (document.getElementById("rockBtn").onclick = function () {
+  playRoundPrintValue("rock");
+});
 
-    console.log("You: " + playerSelection);
-    console.log("Computer: " + computerSelection);
-    console.log("Your Score: ", playerScore);
-    console.log("Computer Score: ", computerScore);
-    console.log("---");
-  }
+let btnPaper = (document.getElementById("paperBtn").onclick = function () {
+  playRoundPrintValue("paper");
+});
 
-  console.log("Player Score: ", playerScore);
-  console.log("Computer Score: ", computerScore);
+let btnScissors = (document.getElementById("scissorsBtn").onclick =
+  function () {
+    playRoundPrintValue("scissors");
+  });
 
-  if (playerScore > computerScore) {
-    console.log(playerWins);
-  } else if (playerScore < computerScore) {
-    console.log(computerWins);
-  } else {
-    console.log(keepPlaying);
+// Announce Winner
+
+function announceWinner() {
+  if (playerScore === 5) {
+    console.log("You win!");
+  } else if (computerScore === 5) {
+    console.log("Computer wins!");
   }
 }
 
-game();
